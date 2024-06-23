b2de7e9db5da16299c31ac328defb7ca082c1a3f
lml <m.luo1900@gmail.com>
2024-06-24 00:13:26 +0800

    add obstacle

--- main.go
+++ main.go
@@ -4,13 +4,16 @@ import (
 	pb "env_middleware/grpc_env_service"
 	"env_middleware/service"
 	"flag"
+	"log"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"log"
 )
 
 var (
-	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	addr = flag.String("addr", "localhost:50052", "the address to connect to")
+	//addr = flag.String("addr", "10.134.92.104:50052", "the address to connect to")
+	//addr = flag.String("addr", "10.134.114.97:50052", "the address to connect to")
 )
 
 func main() {
@@ -23,12 +26,15 @@ func main() {
 	defer conn.Close()
 	c := pb.NewEnvironmentDataClient(conn)
 
-	//request := service.MakeStaticDataRequest(34.46, 78.41, 34.79, 78.74, 14, data.DEM_DATA_TYPE)
-	//service.CallGetStaticDataRequestRPC(c, request)
+	// request := service.MakeStaticDataRequest(34.46, 78.41, 34.79, 78.74, 14, data.DEM_DATA_TYPE)
+	// service.CallGetStaticDataRequestRPC(c, request)
 
 	//crater := service.MakeCrater(78.45, 34.49, 5.33, 4.32)
 	//service.CallUpdateCrater(c, crater)
 
-	startStopPoints := service.MakeStartStopPoints(113.5439372, 22.2180642, 113.5425177, 22.2252363)
-	service.CallGetRoutePoints(c, startStopPoints)
+	//startStopPoints := service.MakeStartStopPoints(113.5439372, 22.2180642, 113.5425177, 22.2252363)
+	//service.CallGetRoutePoints(c, startStopPoints)
+
+	obstacle := service.MakeObstacle(113.416793, 22.158472, "road attack")
+	service.CallUpdateObstacles(c, obstacle)
 }
