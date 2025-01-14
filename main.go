830e5cf3c2fd72a0c4e10fb468a9b4415baac466
LML-1900 <m.luo1900@gmail.com>
2025-01-14 17:19:48 +0800

    socket service with long connection

--- main.go
+++ main.go
@@ -2,27 +2,21 @@ package main
 
 import (
 	"context"
-	"env_middleware/data"
+	"encoding/binary"
+	"encoding/json"
 	pb "env_middleware/grpc_env_service"
 	"env_middleware/service"
 	"flag"
 	"fmt"
 	"log"
-	"unsafe"
+	"net"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
-
-// #include <stdio.h>
-// #include <stdlib.h>
-//
-// static void myprint(char* s) {
-//   printf("%s\n", s);
-// }
-import "C"
 
 var (
 	rdb        *redis.Client
@@ -38,7 +32,7 @@ var consumerCtx context.Context
 var cancelConsumer context.CancelFunc
 
 //export InitMiddleware
-func InitMiddleware(serverUrl, redisUrl, rabbitmqUrl *C.char) {
+func InitMiddleware(serverUrl, redisUrl, rabbitmqUrl string) {
 	// 初始化 stopSignal 和 context
 	stopSignal = make(chan struct{})
 	consumerCtx, cancelConsumer = context.WithCancel(context.Background())
@@ -52,10 +46,10 @@ func InitMiddleware(serverUrl, redisUrl, rabbitmqUrl *C.char) {
 
 	// set Redis
 	var redisUrlString string
-	if redisUrl == nil {
+	if redisUrl == "" {
 		redisUrlString = viper.GetString("redis.url")
 	} else {
-		redisUrlString = C.GoString(redisUrl)
+		redisUrlString = redisUrl
 	}
 	ctx := context.Background()
 	rdb = redis.NewClient(&redis.Options{
@@ -73,10 +67,10 @@ func InitMiddleware(serverUrl, redisUrl, rabbitmqUrl *C.char) {
 
 	// set RabbitMQ
 	var rabbitmqUrlString string
-	if rabbitmqUrl == nil {
+	if rabbitmqUrl == "" {
 		rabbitmqUrlString = viper.GetString("rabbitmq.url")
 	} else {
-		rabbitmqUrlString = C.GoString(rabbitmqUrl)
+		rabbitmqUrlString = rabbitmqUrl
 	}
 	mq, err = service.RunRabbitMqConsumer("dynamic_data_topic", rdb, rabbitmqUrlString)
 	if err != nil {
@@ -89,10 +83,10 @@ func InitMiddleware(serverUrl, redisUrl, rabbitmqUrl *C.char) {
 
 	// connect to server
 	var addr string
-	if serverUrl == nil {
+	if serverUrl == "" {
 		addr = viper.GetString("grpc.addr")
 	} else {
-		addr = C.GoString(serverUrl)
+		addr = serverUrl
 	}
 	flag.Parse()
 	// Set up a connection to the server.
@@ -126,133 +120,136 @@ func CloseMiddleware() {
 	conn.Close()
 }
 
-//export GetRawData
-func GetRawData(minLat, minLon, maxLat, maxLon C.double, level C.int) {
-	request := service.MakeStaticDataRequest(float64(minLat), float64(minLon), float64(maxLat), float64(maxLon), int(level), data.DEM_DATA_TYPE)
-	s.CallGetStaticDataRequestRPC(grpcClient, request)
-}
-
-// 导出为 C 语言兼容的结构体
-type C_LonLatPosition struct {
-	Longitude C.double
-	Latitude  C.double
-}
-
-// // export GetRoutePoints
-// func GetRoutePoints(startLatitude, startLongitude, stopLatitude, stopLongitude C.double) (*C_LonLatPosition, C.int) {
-// 	startStopPoints := service.MakeStartStopPoints(startLongitude, startLatitude, stopLongitude, stopLatitude)
-// 	message := service.CallGetRoutePoints(grpcClient, startStopPoints)
-// 	// 分配 C 语言兼容的数组
-// 	cPositions := C.malloc(C.size_t(len(message.Pos)) * C.size_t(unsafe.Sizeof(C_LonLatPosition{})))
-// 	for i, pos := range message.Pos {
-// 		cPositions[i] = C_LonLatPosition{
-// 			Longitude: C.double(pos.Longitude),
-// 			Latitude:  C.double(pos.Latitude),
-// 		}
-// 	}
-// 	// 返回指向数组第一个元素的指针，以及数组的长度
-// 	return (*C_LonLatPosition)(unsafe.Pointer(&cPositions[0])), C.int(len(cPositions))
-// }
-
-//export GetRoutePoints
-func GetRoutePoints(startLongitude, startLatitude, stopLongitude, stopLatitude C.double) (C.int, *C.double, *C.double) {
-	startStopPoints := service.MakeStartStopPoints(float64(startLongitude), float64(startLatitude), float64(stopLongitude), float64(stopLatitude))
-	fmt.Printf("start: (%f, %f) stop: (%f, %f)\n", startStopPoints.Start.Longitude, startStopPoints.Start.Latitude, startStopPoints.End.Longitude, startStopPoints.End.Latitude)
-	message := service.CallGetRoutePoints(grpcClient, startStopPoints)
-	if message == nil {
-		return 0, nil, nil
-	}
-	length := len(message.Pos)
-	longitude_arr := (*[1 << 30]C.double)(C.malloc(C.size_t(length) * C.size_t(unsafe.Sizeof(C.double(0)))))
-	latitude_arr := (*[1 << 30]C.double)(C.malloc(C.size_t(length) * C.size_t(unsafe.Sizeof(C.double(0)))))
-
-	for i, pos := range message.Pos {
-		longitude_arr[i] = C.double(pos.Longitude)
-		latitude_arr[i] = C.double(pos.Latitude)
-	}
-	return C.int(length), &longitude_arr[0], &latitude_arr[0]
-}
-
-//export FreePositionsPointer
-func FreePositionsPointer(longitude_arr *C.double, latitude_arr *C.double) {
-	C.free(unsafe.Pointer(longitude_arr))
-	C.free(unsafe.Pointer(latitude_arr))
-}
-
-//export UpdateObstacle
-func UpdateObstacle(latitude, longitude C.double, cause *C.char) {
-	obstacle := service.MakeObstacle(float64(longitude), float64(latitude), C.GoString(cause))
-	service.CallUpdateObstacles(grpcClient, obstacle)
-}
-
-//export UpdateCrater
-func UpdateCrater(longitude, latitude, width, depth C.double) {
-	crater := service.MakeCrater(float64(longitude), float64(latitude), float64(width), float64(depth))
-	service.CallUpdateCrater(grpcClient, crater)
-}
-
-//export GetAltitude
-func GetAltitude(longitude, latitude C.double) C.double {
-	altitude, err := s.GetAltitude(float64(longitude), float64(latitude))
-	if err != nil {
-		fmt.Printf("fail to GetAltitude, err: %s\n", err)
-		return 0
-	}
-	return C.double(altitude)
-}
-
 func main() {
 
-	// // test InitMiddleware
-	// var serverUrl *C.char = C.CString("10.134.114.97:50052")
-	// var rabbitmqUrl *C.char = C.CString("amqp://guest:guest@10.134.114.97:5672")
-	// // var redisUrl *C.char = C.CString("10.134.114.97:6379")
-	// defer C.free(unsafe.Pointer(serverUrl))
-	// defer C.free(unsafe.Pointer(rabbitmqUrl))
-	// InitMiddleware(serverUrl, nil, rabbitmqUrl)
+	// test InitMiddleware
+	serverUrl := "10.134.114.218:50052"
+	rabbitmqUrl := "amqp://guest:guest@10.134.114.218:5672"
+	InitMiddleware(serverUrl, "", rabbitmqUrl)
 
-	// // test GetRawData
-	// // GetRawData(34.46, 78.41, 34.47, 78.42, 14)
-
-	// // test GetRoutePoints
-	// len, latitude_arr, longitude_arr := GetRoutePoints(113.5439372, 22.2180642, 113.5425177, 22.2252363)
-	// c_latitude_arr := (*[1 << 30]C.double)(unsafe.Pointer(latitude_arr))
-	// c_longitude_arr := (*[1 << 30]C.double)(unsafe.Pointer(longitude_arr))
-	// for i := 0; i < int(len); i++ {
-	// 	fmt.Printf("lon:%f - lat:%f\n", float64(c_longitude_arr[i]), float64(c_latitude_arr[i]))
-	// }
-	// FreePositionsPointer(longitude_arr, latitude_arr)
-
-	// // test UpdateObstacle
-	// var cause *C.char = C.CString("road attack")
-	// defer C.free(unsafe.Pointer(cause))
-	// UpdateObstacle(113.416793, 22.158472, cause)
-
-	// // test UpdateCrater
-	// UpdateCrater(78.45, 34.49, 5.33, 4.32)
-
-	// // test GetAltitude
-	// altitude := GetAltitude(78.45, 34.39)
-	// fmt.Printf("altitude at lonlat(%f, %f) is %f", 78.45, 34.39, float64(altitude))
-	// CloseMiddleware()
-
-	// request := service.MakeStaticDataRequest(34.46, 78.41, 34.79, 78.74, 14, data.DEM_DATA_TYPE)
-	//request := service.MakeStaticDataRequest(34.46, 78.41, 34.491, 78.448, 14, data.DEM_DATA_TYPE)
-	// request := service.MakeStaticDataRequest(34.46, 78.41, 34.47, 78.42, 14, data.DEM_DATA_TYPE)
-	// s.CallGetStaticDataRequestRPC(c, request)
-
-	// crater := service.MakeCrater(78.45, 34.49, 5.33, 4.32)
-	// service.CallUpdateCrater(c, crater)
+	// 监听 TCP 端口
+	listener, err := net.Listen("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+	fmt.Println("Server is listening on 127.0.0.1:8080")
 
 	// startStopPoints := service.MakeStartStopPoints(113.5439372, 22.2180642, 113.5425177, 22.2252363)
-	// service.CallGetRoutePoints(c, startStopPoints)
+	// fmt.Printf("start: (%f, %f) stop: (%f, %f)\n", startStopPoints.Start.Longitude, startStopPoints.Start.Latitude, startStopPoints.End.Longitude, startStopPoints.End.Latitude)
+	// message := service.CallGetRoutePoints(grpcClient, startStopPoints)
+	// if message == nil {
+	// 	fmt.Println("no route!")
+	// } else {
+	// 	fmt.Printf("got %d points\n", len(message.Pos))
+	// }
 
-	// obstacle := service.MakeObstacle(113.416793, 22.158472, "road attack")
-	// service.CallUpdateObstacles(c, obstacle)
+	for {
+		// 接受客户端连接
+		socket_conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleConnection(socket_conn) // 处理客户端连接
+	}
+}
 
-	// mq.ConsumeMsgs()
-	// var forever chan struct{}
-	// <-forever
-	// altitude, err := s.GetAltitude(78.45, 34.39)
-	// fmt.Printf("altitude: %v", altitude)
+type Request struct {
+	Function string     `json:"function"`
+	Start    [2]float64 `json:"start"`
+	End      [2]float64 `json:"end"`
+}
+
+type Response struct {
+	Length int          `json:"length"`
+	Path   [][2]float64 `json:"path"`
+}
+
+func handleConnection(socket_conn net.Conn) {
+	defer socket_conn.Close()
+	fmt.Println("Client connected:", socket_conn.RemoteAddr())
+
+	buffer := make([]byte, 1024)
+	for {
+		// 设置读取超时时间，例如 5 分钟
+		socket_conn.SetReadDeadline(time.Now().Add(5 * time.Minute))
+
+		n, err := socket_conn.Read(buffer)
+		if err != nil {
+			if err, ok := err.(net.Error); ok && err.Timeout() {
+				fmt.Println("Read timeout, closing connection:", socket_conn.RemoteAddr())
+			} else if err.Error() == "EOF" {
+				fmt.Println("Client disconnected:", socket_conn.RemoteAddr())
+			} else {
+				fmt.Println("Error reading data:", err)
+			}
+			return
+		}
+
+		// 解析请求和处理逻辑
+		var req Request
+		err = json.Unmarshal(buffer[:n], &req)
+		if err != nil {
+			fmt.Println("Error decoding JSON:", err)
+			continue
+		}
+
+		var resp Response
+		if req.Function == "routePlanning" {
+			startStopPoints := service.MakeStartStopPoints(req.Start[0], req.Start[1], req.End[0], req.End[1])
+			message := service.CallGetRoutePoints(grpcClient, startStopPoints)
+			path := make([][2]float64, 0)
+			if message != nil {
+				path = make([][2]float64, len(message.Pos))
+				for i, pos := range message.Pos {
+					path[i][0] = pos.Longitude
+					path[i][1] = pos.Latitude
+				}
+			}
+			resp = Response{
+				Length: len(path),
+				Path:   path,
+			}
+		} else {
+			resp = Response{
+				Length: 0,
+			}
+		}
+
+		// 发送响应
+		respData, err := json.Marshal(resp)
+		if err != nil {
+			fmt.Println("Error encoding JSON:", err)
+			continue
+		}
+
+		err = sendMessage(socket_conn, respData)
+		if err != nil {
+			fmt.Println("Error sending response:", err)
+			return
+		}
+	}
+}
+
+func sendMessage(conn net.Conn, data []byte) error {
+	// Step 1: 构造头部
+	header := make([]byte, 4) // 4字节头部
+	messageSize := len(data)
+	binary.BigEndian.PutUint32(header, uint32(messageSize)) // 网络字节序
+
+	// Step 2: 发送头部
+	_, err := conn.Write(header)
+	if err != nil {
+		return fmt.Errorf("failed to send header: %w", err)
+	}
+
+	// Step 3: 发送消息体
+	_, err = conn.Write(data)
+	if err != nil {
+		return fmt.Errorf("failed to send message body: %w", err)
+	}
+
+	return nil
 }
